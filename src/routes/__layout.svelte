6092f9cd7c1a748f48380e56d19a5b87a95d9d29
jakequinter <hello@jakequinter.io>
2021-07-01 12:05:57 -0400

    added tailwindcss

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Container from '../components/Container.svelte';
+</script>
+
+<Container />
+
+<!-- The slot refers to the children -->
+<slot></slot>
+
+<style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
