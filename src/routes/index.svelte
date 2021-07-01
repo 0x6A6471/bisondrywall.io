7b68dacb152216768cc3e8713d6b758014e32d88
jakequinter <hello@jakequinter.io>
2021-07-01 15:03:30 -0400

    added linear background and added Facebook link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,11 +6,27 @@
   <title>Home</title>
 </svelte:head>
 
-<div class="flex flex-col justify-center items-center h-screen bg-gray-300">
-  <p class="text-2xl text-gray-600 mb-16">Coming soon...</p>
+<div class="container flex flex-col justify-center items-center h-screen">
+  <p class="text-2xl text-gray-400 mb-16">Coming soon...</p>
   <img class="h-80" src="/logo.png" alt="logo" />
-  <FacebookIcon
-    class="mt-16 text-[#1877f2] hover:text-blue-500 cursor-pointer"
-    size="48"
-  />
+  <div class="flex items-center text-gray-500 mt-16 text-lg">
+    <span>Find us on</span>
+    <a
+      class="flex items-center hover:underline cursor-pointer"
+      href="https://m.facebook.com/bisondrywall/"
+    >
+      <FacebookIcon class="text-[#1877f2] -mr-1" size="20" />
+      <span>acebook</span>
+    </a>
+  </div>
 </div>
+
+<style>
+  .container {
+    background: linear-gradient(
+      180deg,
+      rgba(62, 62, 62, 1) 0%,
+      rgba(86, 86, 86, 1) 50%
+    );
+  }
+</style>
