e1a1a490e4cd1bf76c180a2c35859ec16af7268a
jakequinter <hello@jakequinter.io>
2021-07-01 14:47:21 -0400

    Fixing image location

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 
 <div class="flex flex-col justify-center items-center h-screen bg-gray-300">
   <p class="text-2xl text-gray-600 mb-16">Coming soon...</p>
-  <img class="h-80" src="static/logo.png" alt="logo" />
+  <img class="h-80" src="/logo.png" alt="logo" />
   <FacebookIcon
     class="mt-16 text-[#1877f2] hover:text-blue-500 cursor-pointer"
     size="48"
