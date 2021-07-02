77c0db5b839ea1c1789a2037f4b74edc72ebf414
jakequinter <hello@jakequinter.io>
2021-07-02 15:57:37 -0400

    add custom 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Container from '../components/Container.svelte';
+</script>
+
+<Container>
+  <div class="text-center">
+    <h1 class="text-2xl md:text-3xl lg:text-4xl mb-4">
+      Oops, looks like this page no longer exists
+    </h1>
+    <p>
+      If you think something is missing, please reach out to our <a
+        class="text-blue-500 underline"
+        href="mailto:bisondrywallllc@gmail.com">support</a
+      > team.
+    </p>
+  </div>
+</Container>
