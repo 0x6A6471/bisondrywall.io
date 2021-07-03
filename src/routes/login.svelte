f3bac16553ddd603ea7a91454baee8a4d872879a
jakequinter <hello@jakequinter.io>
2021-07-03 16:27:35 -0400

    added Supabase database and login page

---
+++ src/routes/login.svelte
@@ -0,0 +1,14 @@
+<script>
+  import supabase from '../lib/db';
+
+  async function signUp() {
+    // Create a new user
+    const { user, error } = await supabase.auth.signUp({
+      email: 'hello@jakequinter.io',
+      password: 'example-password',
+    });
+    console.log(user, error);
+  }
+</script>
+
+<button on:click={signUp}>Login in</button>
