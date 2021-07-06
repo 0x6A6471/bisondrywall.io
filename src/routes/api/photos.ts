7999f3c23338981a58962f5ebddeb9d372c49556
jakequinter <hello@jakequinter.io>
2021-07-06 14:49:28 -0400

    moved Graph API logic to photos API

--- src/routes/api/photos.ts
+++ src/routes/api/photos.ts
@@ -1,15 +1,23 @@
-import supabase from '$lib/db';
-
 /**
  * @type {import('@sveltejs/kit').RequestHandler}
  */
 export async function get() {
 
-	const photos = await supabase.from('photos').select(`id, src, alt`);
+	const res = await fetch(
+		'https://graph.facebook.com/112096071040835/photos?limit=100&fields=link,alt_text,images',
+		{
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${import.meta.env.VITE_FACEBOOK_ACCESS_TOKEN}`,
+			},
+		}
+	);
+
+	const photos = await res.json();
 
 		return {
 			body: {
-				photos
+				photos: photos
 			}
 		};
 	
