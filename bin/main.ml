e4a21636919e4e6d590a7033665797485629dec7
Jake Quinter <hello@jakequinter.io>
2023-10-13 12:41:01 -0500

    jumbotron

--- bin/main.ml
+++ bin/main.ml
@@ -4,14 +4,43 @@ let index_body _ =
   let open Tyxml in
   Html.(
     div
-      [ h1
-          ~a:[ a_class [ "text-4xl text-pink-500 font-semibold" ] ]
-          [ txt "Bison Drywall" ]
-      ; img
-          ~a:[ a_width 94; a_height 40 ]
-          ~src:"/assets/logo.png"
-          ~alt:"Bison Image"
-          ()
+      ~a:[ a_class [ "jumbotron" ] ]
+      [ div
+          ~a:[ a_class [ "p-4" ] ]
+          [ img
+              ~a:[ a_width 94; a_height 40 ]
+              ~src:"/assets/logo.png"
+              ~alt:"Bison Image"
+              ()
+          ]
+      ; div
+          ~a:[ a_class [ "px-8 pt-24 text-center sm:px-0" ] ]
+          [ h1
+              ~a:
+                [ a_class
+                    [ "text-4xl font-bold text-white sm:text-5xl md:text-7xl" ]
+                ]
+              [ txt "Bison Drywall" ]
+          ; h2
+              ~a:
+                [ a_class [ "mt-2 text-lg text-white opacity-50 md:text-2xl" ] ]
+              [ txt "Whatever your needs, Bison Drywall can help" ]
+          ; a
+              ~a:
+                [ a_href "mailto:bisondrywalllc.com"
+                ; a_class
+                    [ "btn-grad mt-8 inline-flex items-center rounded-xl px-8 \
+                       py-4 font-medium text-white md:mt-16"
+                    ]
+                ]
+              [ txt "Contact Us"
+              ; img
+                  ~a:[ a_class [ "ml-4" ]; a_width 20; a_height 20 ]
+                  ~src:"/assets/paper_plane.svg"
+                  ~alt:"paper plane"
+                  ()
+              ]
+          ]
       ])
 ;;
 
@@ -21,7 +50,9 @@ let index _ =
     html
       (head
          (title (txt "Bison Drywall"))
-         [ script ~a:[ a_src "https://cdn.tailwindcss.com" ] (txt "") ])
+         [ link ~rel:[ `Stylesheet ] ~href:"/styles/global.css" ()
+         ; script ~a:[ a_src "https://cdn.tailwindcss.com" ] (txt "")
+         ])
       (body [ index_body () ])
   in
   Dream.html (elt_to_string page)
@@ -33,6 +64,7 @@ let () =
   @@ Dream.router
        [ Dream.get "/" (fun _ -> index ())
        ; Dream.get "/about" (fun _ -> Dream.html "About")
+       ; Dream.get "/styles/**" @@ Dream.static "./styles"
        ; Dream.get "/assets/**" @@ Dream.static "./assets"
        ]
 ;;
