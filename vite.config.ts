5a1d4736bd2909526352f20733435a8576ce4760
0x6A6471 <hello@jakequinter.io>
2025-10-15 11:46:08 -0500

    fix: allow multiple routes via vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
   publicDir: '../public',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        privacy: resolve(__dirname, 'src/privacy.html')
+      }
+    }
   },
   plugins: [tailwindcss()]
 })
